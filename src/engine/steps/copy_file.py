3789379b444371df0900eb5a346c913fbeabe87b
TristanODonnell <161791230+TristanODonnell@users.noreply.github.com>
2026-02-16 17:46:00 -0800

    step type module stubs

---
+++ src/engine/steps/copy_file.py
@@ -0,0 +1,5 @@
+# copy_file.py
+
+class CopyFileStep:
+    def run(self, params):
+        raise NotImplementedError("CopyFileStep not implemented yet")
---
+++ src/engine/steps/write_file.py
@@ -0,0 +1,6 @@
+# write_file.py
+
+class WriteFileStep:
+    def run(self, params):
+        raise NotImplementedError("WriteFileStep not implemented yet")
+
